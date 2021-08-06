c4849909bd8942807694c724a9a5162a1dfa3f66
Pratik Kumar <pratikkumarsbg@gmail.com>
2021-08-07 00:33:12 +0530

    initial commit

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,24 +1,32 @@
 import { Component } from "@angular/core";
+import { CoursesService } from "./courses.service";
 
 @Component({
     selector: 'courses',    //defines the tag name//a tag with name 'courses' will be created
     template: `
+    <img [src]="imageUrl"/>
         <h2>{{ title }}</h2>
         <ul>
             <li *ngFor="let i of courses">
             {{ i }}
             </li>
         </ul>
+
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan">1</td>
+            </tr>
+        </table>
     `   //looping on courses
 })
 export class CoursesComponent {
     title = "List of Courses";
-    courses = ["Course1", "Course2", "Course3"];
+    imageUrl = 'https://www.w3schools.com/angular/pic_angular.jpg';
+    colSpan = 2;
+    courses;
 
-    /*
-    getTitle() {
-        return this.title;
+    constructor(service: CoursesService) {
+        //let service = new CoursesService();
+        this.courses = service.getCourses();
     }
-    */
-
 }
